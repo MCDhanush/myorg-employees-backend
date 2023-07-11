6ace0cf1a0dbe3de23bbaef1a10ae61d162a6cc4
Dhanush MC <dhanushmc93@gmail.com>
2023-07-11 14:12:05 +0530

    Publish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const db= require("./db/connect.js")
+import cors from "cors"
 
 // import route
 const employeesRoutes = require("./routes/employee.route.js")
@@ -14,7 +15,7 @@ app.get(("/"),(req,res)=>{
 
 // import connection
 db()
-
+app.use(cors())
 
 // middleware
 app.use(express.json())
